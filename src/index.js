ed73ce94088d5d27441c9e3c807dfaf902a0529f
Eugene <levchuk.eugene@gmail.com>
2018-09-24 18:33:03 +0300

    solution-quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let str = equation.split(" ")
+  let solArr = [];
+  let a = str[0]
+  let b = (str[3] + str[4])
+  let c = (str[7] + str[8])
+  let d = Math.round(b * b - 4 * a * c)
+  solArr[0] = Math.round((-b + Math.sqrt(d)) / (2 * a))
+  solArr[1] = Math.round((-b - Math.sqrt(d)) / (2 * a))
+
+  return (solArr.sort((a, b) => a - b))
 }
